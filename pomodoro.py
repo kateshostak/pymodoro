e6517b0ae5329526de5bcced6b3a25b71dbd705b
trina <31892809+kateshostak@users.noreply.github.com>
2019-04-12 14:40:36 +0300

    Make use of asyncio

--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,5 @@
 import time
-
+import asyncio
 
 class Pomodoro(object):
     def __init__(self, work_time, short_break, long_break, long_break_frequency):
@@ -8,15 +8,15 @@ class Pomodoro(object):
         self.long_break = long_break
         self.long_break_frequency = long_break_frequency
 
-    def start(self):
+    async def start(self):
         for i in range(self.long_break_frequency):
-            self.start_activity('work')
+            await self.start_activity('work')
             if i != self.long_break_frequency - 1:
-                self.start_activity('break')
+                await self.start_activity('break')
             else:
-                self.start_activity('long_break')
+                await self.start_activity('long_break')
 
-    def start_activity(self, activity):
+    async def start_activity(self, activity):
         if activity == 'work':
             print('work!')
             activity_time_min = self.work_time
@@ -27,16 +27,16 @@ class Pomodoro(object):
             print('long break!')
             activity_time_min = self.long_break
 
-        self.start_counter(activity_time_min)
+        await self.start_counter(activity_time_min)
 
-    def start_counter(self, activity_time_min):
+    async def start_counter(self, activity_time_min):
         activity_time_sec = activity_time_min*60
         for sec in range(activity_time_sec):
             seconds = sec % 60
             minutes = (sec // 60) % 60
             hours = (sec)//3600
             print(f'{hours}:{minutes}:{seconds}', end='\r')
-            time.sleep(1/50)
+            await asyncio.sleep(1)
 
 
 def main():
@@ -44,9 +44,10 @@ def main():
     short_break = int(input('Set the short rest time in minutes: ').strip())
     long_break = int(input('Set the long break time in minutes: ').strip())
     long_break_frequency = int(input('Set the long break frequency in minutes: ').strip())
-
+    loop = asyncio.get_event_loop()
     pomodoro = Pomodoro(work_time, short_break, long_break, long_break_frequency)
-    pomodoro.start()
+    loop.run_until_complete(pomodoro.start())
+
 
 
 if __name__ == '__main__':
