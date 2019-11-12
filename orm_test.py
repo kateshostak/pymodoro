11cc6f505b9824f93848a60218b866265a2a7765
trina <31892809+kateshostak@users.noreply.github.com>
2019-11-12 18:55:21 +0300

    Add the test for user deletion

--- orm_test.py
+++ orm_test.py
@@ -3,12 +3,14 @@ import os
 from orm import ORM
 from user import User
 
+
 def get_orm_test(type_, path):
     db = get_db(type_, path)
     os.remove(path)
     if db.type == type_:
         return True
     return False
+
 
 def create_user_test(type_, path, user):
     db = get_db(type_, path)
@@ -19,6 +21,7 @@ def create_user_test(type_, path, user):
         return True
     return False
 
+
 def get_user_test(type_, path, user):
     db = get_db(type_, path)
     db.create_user(user)
@@ -28,30 +31,58 @@ def get_user_test(type_, path, user):
         return True
     return False
 
+
 def get_db(type_, path):
     orm = ORM()
     return orm.get_orm(type_, path)
 
+
 def test_manager(types, paths, users):
     for type_, path, user in zip(types, paths, users):
         if not get_orm_test(type_, path):
-            print(f'get_orm_test failed with type:{type_}, db:{path}')
+            print(f'get_orm_test failed with type:{type_}, db:{path}') # noqa
         if not get_user_test(type_, path, user):
             print(f'get_user_test failed with type:{type_}, user{user}, db:{path}')
         if not create_user_test(type_, path, user):
            print(f'create_user_test failed with type:{type_}, user:{user}, db:{path}')
+        if not delete_user_test(type_, path, user):
+            print(f'delete_user_test failed with type:{type_}, user:{user}, db:{path}')
+
+
+def delete_user_test(type_, path, user):
+    db = get_db(type_, path)
+    db.create_user(user)
+    res = db.delete_user(user.name)
+    if res:
+        return True
+    return False
+
+
+def update_user_test(type_, path, user, updated_user):
+    db = get_db(type_, path)
+    db.create_user(user)
+    db.update_user(user.name, params)
+    db_user = db.get_user(user.name, 'work')
+    if str(updated_user) == str(db_user):
+        return True
+    return False
+
 
 def delete_test_dbs(paths):
     for path in paths:
         os.remove(path)
 
+
 def main():
-    sqlite_user = User(1,'test', '25', '5', '10', '4')
-    json_user= User(None,'test', '25', '5', '10', '4')
+    sqlite_user = User(1, 'test', '25', '5', '10', '4')
+    json_user = User(None, 'test', '25', '5', '10', '4')
     users = [sqlite_user, json_user]
     types = ['sqlite', 'json']
-    paths_to_dbs = ['test_pom.db','test_data.json']
+    paths_to_dbs = ['test_pom.db', 'test_data.json']
+    updated_sqlite_user = User(1, 'test', '30', '30', '30', '4')
+    updated_json_user= User(None, 'test', '30', '30', '30', '4')
     test_manager(types, paths_to_dbs, users)
+
 
 if __name__ == '__main__':
     main()
