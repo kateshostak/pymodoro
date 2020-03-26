2743180f8c6b4e563c36d532fa59ef5e50f24fc5
trina <31892809+kateshostak@users.noreply.github.com>
2020-03-26 17:41:06 +0300

    wip

--- xml_orm.py
+++ xml_orm.py
@@ -6,42 +6,48 @@ class XmlORM(object):
     def __init__(self, path_to_db):
         self.type = 'xml'
         self.path_to_db = path_to_db
+        self.create_db()
 
     def create_db(self):
         try:
             with open(self.path_to_db, 'r'):
                 pass
-        except FileNotFoundError:
+        except FileNotFoundError: # noqa
             with open(self.path_to_db, 'w'):
                 pass
+
+    def udate_user(self, user):
+        pass
 
     def create_user(self, user):
         users = self.load_xml()
         if user.name in users:
             return False
-
         else:
-            top = ET.Element('data')
+            top = ET.Element('users')
             usr = top.SubElement(top, 'user')
             usr.set('name', user.name)
             setting = top.SubElement(usr, 'setting')
             setting.set('setting', user.setting)
 
-
     def load_xml(self):
-        users = ET.parse(self.path_to_db).getroot()
-        return {user.find('name').text:user for user in users}
+        try:
+            users = ET.parse(self.path_to_db).getroot()
+            return {user.find('name').text: user for user in users}
+        except ET.ParseError:
+            return []
 
     def user_constructor(self, user_elem, setting):
-        settings = {setting.find('name').text:setting for setting in user.elem.findall('setting')}
+        settings = {setting.find('name').text: setting for setting in user_elem.findall('setting')} # noqa
         if setting not in settings:
             return False, [name for name in settings]
         else:
             name = user_elem.find('name').text
-            work = settings[setting].find('work')
-            shortbreak = settings[setting].find('shortbreak')
-            longbreak = settings[setting].find('longbreak')
-            cycle = settings[setting].find('cycle')
+            profile = settings[setting]
+            work = int(profile.find('work').text)
+            shortbreak = int(profile.find('shortbreak').text)
+            longbreak = int(profile.find('longbreak').text)
+            cycle = int(profile.find('cycle').text)
         return User(None, name, work, shortbreak, longbreak, cycle)
 
     def get_user(self, name, setting):
@@ -49,5 +55,4 @@ class XmlORM(object):
         if name not in users:
             return False
         else:
-            return user_constructor(users[name], setting)
-
+            return self.user_constructor(users[name], setting)
