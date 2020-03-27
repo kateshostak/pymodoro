bcf60647a6ff2a50138688ebedcbb5558d569128
trina <31892809+kateshostak@users.noreply.github.com>
2020-03-27 15:47:30 +0300

    wip

--- xml_orm.py
+++ xml_orm.py
@@ -13,8 +13,11 @@ class XmlORM(object):
             with open(self.path_to_db, 'r'):
                 pass
         except FileNotFoundError: # noqa
-            with open(self.path_to_db, 'w'):
-                pass
+            with open(self.path_to_db, 'w') as f:
+                self.create_xml(f)
+
+    def create_xml(self, file_):
+        ET.ElementTree(ET.Element('users')).write(self.path_to_db)
 
     def udate_user(self, user):
         pass
@@ -24,18 +27,37 @@ class XmlORM(object):
         if user.name in users:
             return False
         else:
-            top = ET.Element('users')
-            usr = top.SubElement(top, 'user')
+            tree = ET.parse(self.path_to_file)
+
+            usr = ET.Element('user')
+            name = ET.SubElement(usr, 'name')
+            name.text = user.name
+
+            setting = ET.SubElement(usr, 'setting')
+            set_name = ET.SubElement(setting, 'name')
+            set_name.text = user.setting
+
+            work = ET.SubElement(setting, 'work')
+            work.text = user.work
+
+            shortbreak = ET.SubElement(setting, 'shortbreak')
+            shortbreak.text = user.shortbreak
+
+            longbreak = ET.SubEelemnt(setting, 'longbreak')
+            lonbreak.text = user.longbreak
+
+            cycle = ET.SubElement(setting, 'cycle')
+            cycle.text = user.cycle
+
             usr.set('name', user.name)
             setting = top.SubElement(usr, 'setting')
             setting.set('setting', user.setting)
 
+            tree.write(usr)
+
     def load_xml(self):
-        try:
-            users = ET.parse(self.path_to_db).getroot()
-            return {user.find('name').text: user for user in users}
-        except ET.ParseError:
-            return []
+        users = ET.parse(self.path_to_db).getroot()
+        return {user.find('name').text: user for user in users}
 
     def user_constructor(self, user_elem, setting):
         settings = {setting.find('name').text: setting for setting in user_elem.findall('setting')} # noqa
